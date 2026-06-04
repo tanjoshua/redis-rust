9461ea0f130b8945326fa1090c08f17852e5bf4f
Josh <34528652+tanjoshua@users.noreply.github.com>
2026-06-03 21:32:31 -0600

    feat: handle set

--- src/server.rs
+++ src/server.rs
@@ -1,18 +1,26 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::RwLock;
 
 use crate::resp::{RESPData, parse_resp};
 
+type Store = Arc<RwLock<HashMap<Vec<u8>, Vec<u8>>>>;
+
 pub async fn run() -> anyhow::Result<()> {
+    let data_store: Store = Arc::new(RwLock::new(HashMap::new()));
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
     loop {
         let (stream, _address) = listener.accept().await?;
-        tokio::spawn(async move { handle_connection(stream).await });
+        let task_data_store = Arc::clone(&data_store);
+        tokio::spawn(async move { handle_connection(stream, task_data_store).await });
     }
 }
 
-async fn handle_connection(mut stream: TcpStream) -> anyhow::Result<()> {
+async fn handle_connection(mut stream: TcpStream, data_store: Store) -> anyhow::Result<()> {
     let mut buf = [0; 512];
     loop {
         let bytes_read = stream.read(&mut buf).await?;
@@ -35,6 +43,16 @@ async fn handle_connection(mut stream: TcpStream) -> anyhow::Result<()> {
             RESPData::BulkString(bulk_string) => match bulk_string.as_slice() {
                 b"PING" => &RESPData::SimpleString(String::from("PONG")),
                 b"ECHO" => &resp_commands[1],
+                b"SET" => {
+                    let [_, RESPData::BulkString(key), RESPData::BulkString(value)] =
+                        resp_commands.as_slice()
+                    else {
+                        anyhow::bail!("SET requires a key and value")
+                    };
+
+                    handle_set(&data_store, key.clone(), value.clone()).await;
+                    &RESPData::SimpleString(String::from("OK"))
+                }
                 _ => &RESPData::SimpleString(String::from("Unimplemented")),
             },
             _ => anyhow::bail!("Invalid RESP command"),
@@ -44,4 +62,8 @@ async fn handle_connection(mut stream: TcpStream) -> anyhow::Result<()> {
     }
     stream.flush().await?;
     Ok(())
+}
+
+async fn handle_set(data_store: &Store, key: Vec<u8>, value: Vec<u8>) {
+    data_store.write().await.insert(key, value);
 }
